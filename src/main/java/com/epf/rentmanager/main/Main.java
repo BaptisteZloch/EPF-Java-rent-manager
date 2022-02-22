2313fe89b410cf2f27cc1d518cb8e2d9fb9aeb14
BaptisteZloch <bzloch@hotmail.fr>
2022-02-22 12:59:15 +0100

    Main creation

---
+++ src/main/java/com/epf/rentmanager/main/Main.java
@@ -0,0 +1,7 @@
+package com.epf.rentmanager.main;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/epf/rentmanager/service/ClientService.java
+++ src/main/java/com/epf/rentmanager/service/ClientService.java
@@ -1,5 +1,6 @@
 package com.epf.rentmanager.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
