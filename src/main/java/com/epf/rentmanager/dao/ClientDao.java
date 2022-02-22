022fb8a260c96d4d68c05d490b477b60d6a95470
BaptisteZloch <bzloch@hotmail.fr>
2022-02-22 10:33:53 +0100

    DAO client ok

--- src/main/java/com/epf/rentmanager/dao/ClientDao.java
+++ src/main/java/com/epf/rentmanager/dao/ClientDao.java
@@ -1,12 +1,10 @@
 package com.epf.rentmanager.dao;
 
-import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -36,52 +34,69 @@ public class ClientDao {
 	private static final String FIND_CLIENTS_QUERY = "SELECT id, nom, prenom, email, naissance FROM Client;";
 
 	public long create(Client client) throws DaoException {
-		
+
 		long key = 0;
-		try(Statement stmt = ConnectionManager.getConnection().prepareStatement(CREATE_CLIENT_QUERY,Statement.RETURN_GENERATED_KEYS)) {
+		try (Statement stmt = ConnectionManager.getConnection().prepareStatement(CREATE_CLIENT_QUERY,
+				Statement.RETURN_GENERATED_KEYS)) {
 			((PreparedStatement) stmt).setString(1, client.getNom());
 			((PreparedStatement) stmt).setString(2, client.getPrenom());
 			((PreparedStatement) stmt).setString(3, client.getEmail());
 			((PreparedStatement) stmt).setDate(4, Date.valueOf(client.getNaissance()));
 			key = ((PreparedStatement) stmt).executeUpdate();
 			return key;
-		  } catch (SQLException e) {
-			//DaoException(e);
-		  }	
-		  return key;	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return key;
 	}
 
 	public long delete(Client client) throws DaoException {
 		long key = 0;
-		try(Statement stmt = ConnectionManager.getConnection().prepareStatement(DELETE_CLIENT_QUERY,Statement.RETURN_GENERATED_KEYS)) {
+		try (Statement stmt = ConnectionManager.getConnection().prepareStatement(DELETE_CLIENT_QUERY,
+				Statement.RETURN_GENERATED_KEYS)) {
 			((PreparedStatement) stmt).setInt(1, client.getId());
 			key = ((PreparedStatement) stmt).executeUpdate();
 			return key;
-		  } catch (SQLException e) {
-			//DaoException(e);
-		  }	
-		  return key;	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return key;
 	}
 
 	public Optional<Client> findById(long id) throws DaoException {
 		Client client;
-		try(Statement stmt = ConnectionManager.getConnection().createStatement()) {
+		try (Statement stmt = ConnectionManager.getConnection().createStatement()) {
 			((PreparedStatement) stmt).setLong(1, id);
 			ResultSet rs = ((PreparedStatement) stmt).executeQuery(FIND_CLIENT_QUERY);
 			while (rs.next()) {
-				client = new Client(rs.getInt("id"),rs.getString("nom"),rs.getString("prenom"),rs.getString("email"),rs.getDate("naissance").toLocalDate());
+				client = new Client(rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"),
+						rs.getDate("naissance").toLocalDate());
 				System.out.println(client);
 				return Optional.ofNullable(client);
 			}
 
-		  } catch (SQLException e) {
-			//DaoException(e);
-		  }	
-		  return  null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public List<Client> findAll() throws DaoException {
+		ArrayList<Client> clientResultList = new ArrayList<Client>();
+		try (Statement stmt = ConnectionManager.getConnection().createStatement()) {
+			ResultSet rs = ((PreparedStatement) stmt).executeQuery(FIND_CLIENTS_QUERY);
+			// Stores properties of a ResultSet object, including column count
+			while (rs.next()) {
+					Client client = new Client(rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"),
+							rs.getString("email"), rs.getDate("naissance").toLocalDate());
+					clientResultList.add(client);
+				
+			}
 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return clientResultList;
 	}
 
 }
