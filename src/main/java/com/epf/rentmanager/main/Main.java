76d39935d4aa4402ec01c7648f79242566cbd594
BaptisteZloch <bzloch@hotmail.fr>
2022-02-22 16:16:41 +0100

    servlet ok

--- src/main/java/com/epf/rentmanager/main/Main.java
+++
@@ -1,7 +0,0 @@
-package com.epf.rentmanager.main;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/com/epf/rentmanager/ui/servlets/HomeServlet.java
+++ src/main/java/com/epf/rentmanager/ui/servlets/HomeServlet.java
@@ -8,10 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "HomeServlet", urlPatterns = "/rentmanager/home")
+@WebServlet(name = "HomeServlet", urlPatterns = "/home")
 public class HomeServlet extends HttpServlet {
-
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("./WEB-INF/views/home.jsp").forward(request, response);
