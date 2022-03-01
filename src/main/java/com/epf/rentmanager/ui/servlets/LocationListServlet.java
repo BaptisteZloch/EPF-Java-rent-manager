c661735684b2eb08121ab883f2e42318bfbc6031
BaptisteZloch <bzloch@hotmail.fr>
2022-03-01 13:15:44 +0100

    rents list ok

---
+++ src/main/java/com/epf/rentmanager/ui/servlets/LocationListServlet.java
@@ -0,0 +1,49 @@
+package com.epf.rentmanager.ui.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.epf.rentmanager.exception.ServiceException;
+import com.epf.rentmanager.model.Vehicle;
+import com.epf.rentmanager.service.VehicleService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+@WebServlet(name = "LocationListServlet", urlPatterns = "/rents")
+public class LocationListServlet extends HttpServlet {
+    @Autowired
+    private VehicleService vehicleService;
+
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+        System.out.println("My servlet has been initialized");
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            ArrayList<Vehicle> vehicle = vehicleService.findAll();
+            request.setAttribute("vehicules", vehicle);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+        request.getRequestDispatcher("./WEB-INF/views/rents/list.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
