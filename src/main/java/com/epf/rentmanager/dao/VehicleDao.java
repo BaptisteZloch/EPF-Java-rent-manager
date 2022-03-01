6656421e321882b363e2a20dd300af0ef952f935
BaptisteZloch <bzloch@hotmail.fr>
2022-03-01 09:48:41 +0100

    vehicule add ok

--- src/main/java/com/epf/rentmanager/dao/VehicleDao.java
+++ src/main/java/com/epf/rentmanager/dao/VehicleDao.java
@@ -33,7 +33,7 @@ public class VehicleDao {
 	private static final String DELETE_VEHICLE_QUERY = "DELETE FROM Vehicle WHERE id=?;";
 	private static final String FIND_VEHICLE_QUERY = "SELECT id, constructeur, modele, nb_places FROM Vehicle WHERE id=?;";
 	private static final String FIND_VEHICLES_QUERY = "SELECT id, constructeur, modele, nb_places FROM Vehicle;";
-	private static final String FIND_VEHICLES_COUNT_QUERY = "SELECT COUNT(id) AS count FROM Vehicule";
+	private static final String FIND_VEHICLES_COUNT_QUERY = "SELECT COUNT(id) AS count FROM Vehicle";
 	
 
 	public long create(Vehicle vehicle) throws DaoException {
